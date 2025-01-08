a3ba8e3966a19d66363b822b8d216323b4ecc2c5
benxyzzy <benxyzzy@fake.fail>
2025-01-08 18:30:48 +0000

    comment out my personal env vars

--- tests/test_concat_ac3.py
+++ tests/test_concat_ac3.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -15,20 +16,24 @@ def stg_filepath():
 
 def test_concat_ac3(stg_filepath, monkeypatch):
     """High-level "smoke test" to check it runs without errors."""
-    FG_ROOT = "/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/"
-    monkeypatch.setenv("FG_ROOT", FG_ROOT)
-    monkeypatch.setattr("concat_ac3.FG_ROOT", FG_ROOT)
-
-    FG_SCENERY = "/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/Scenery/SceneryPack.BIKF/"
-    monkeypatch.setenv("FG_SCENERY", FG_SCENERY)
-    monkeypatch.setattr("concat_ac3.FG_SCENERY", FG_SCENERY)
+    # FG_ROOT = "/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/"
+    # monkeypatch.setenv("FG_ROOT", FG_ROOT)
+    # monkeypatch.setattr("concat_ac3.FG_ROOT", FG_ROOT)
+    #
+    # FG_SCENERY = "/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/Scenery/SceneryPack.BIKF/"
+    # monkeypatch.setenv("FG_SCENERY", FG_SCENERY)
+    # monkeypatch.setattr("concat_ac3.FG_SCENERY", FG_SCENERY)
 
     concat_ac3.main(STGFile_path=stg_filepath)
 
 
 def test_scale_factor(monkeypatch):
     """Run with different SCALE_FACTOR values and compare"""
-    ACFilePath = '/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/Scenery/SceneryPack.BIKF/Models/Airport/thangar.ac'
+    # FG_ROOT = "/home/x/PycharmProjects/flightgear_export_scripts/fg_install/fgdata/fgdata/"
+    # monkeypatch.setenv("FG_ROOT", FG_ROOT)
+
+    FG_ROOT = os.environ["FG_ROOT"]
+    ACFilePath = str(Path(FG_ROOT) / 'Scenery/SceneryPack.BIKF/Models/Airport/thangar.ac')
     splitExtension = ['Models/Airport/thangar', 'ac']
     splitLine = ['OBJECT_SHARED', 'Models/Airport/thangar.ac', '-121.60000000', '37.0810000', '84.0', '204']
 
